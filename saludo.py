33046d9fa11295953645b02e51e5d85bba9de2bc
andresdino <andresdino9ster@gmail.com>
2023-08-08 18:35:20 -0500

    Clase  08/08/2023

--- saludo.py
+++ saludo.py
@@ -1,1 +1,27 @@
 print("Hola mundo")
+
+a = "5" #Entero
+b = 3.5 #Float
+c = "Hola" #String
+d = True
+
+print(a,b,c,d)
+#-----------------------
+a,b,c = 45 , "Hola" , 2.2
+
+print(a,b,c)
+
+#------------------
+numero = 10
+numero_2 = 10
+
+r = numero + numero_2
+
+print(r)
+
+#-------------------
+nombre = "Juan Sanchez"
+
+print("Su nombre es : ", nombre)
+print("Su nombre es : "+ nombre)
+print("Su nombre es : "+ str(nombre))
