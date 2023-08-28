6efe9cfb4793cbfb9791361ce7c4f1220c60644c
andresdino <andresdino9ster@gmail.com>
2023-08-28 18:59:12 -0500

    CArgando electiva

---
+++ Electiva2/Electiva2/src/Main.java
@@ -0,0 +1,66 @@
+public class Main {
+
+    /**
+     *
+     * @param args
+     * Author: Andresdino
+     * Description: this code talks ejejej ajfds
+     */
+    public static void main(String[] args) {
+
+        //Comentarios de una sola linea
+        //camelCase-> variables, metodos y objetos-> objConectar objValidar objContar
+        //PascalCase -> Clases y paquetes
+        /*
+        comentarios
+        multinea
+
+
+        int a  = 5;
+        System.out.println("El resultado es: " + a);
+
+        int b, resultado;
+        b= 10;
+        resultado = a + b;
+        System.out.println("El resultado es: "+ resultado);
+
+        //Operaciones
+        int suma, resta, multipli, div;
+        int num1, num2;
+
+        num1 = 20;
+        num2 = 10;
+
+        suma = num1 + num2;
+        resta = num1 - num2;
+        multipli = num1 * num2;
+        div = num1 / num2;
+
+        System.out.println("Suma: "+ suma + " Resta: "+ resta + " multi: "+ multipli + " Div: "+ div);
+
+
+        float q = 3.5f;
+        double w = 3.5;
+
+        double promedio = (3.5*0.3) + (2.6 * (30/100)) + (4 * 0.40);
+        System.out.println("Mi promedio es :" + promedio);
+        */
+        String nombre = "Hector";
+        String apellido = "Sanchez";
+
+        System.out.println("Nombre :" + nombre + apellido);
+
+
+        boolean t = true;
+        boolean i = false;
+
+
+
+
+
+
+
+
+
+    }
+}
