7b602a0a963795d4b35b8d2f8b6fb0613dadc01e
fabricasw24 <fabricasw24@superfinanciera.gov.co>
2023-10-25 16:59:05 -0500

    Cargando el CRUD completo

--- Electiva2/crudUSCOSpring/src/main/java/crudPersonas/demo/controller/ControllerPersona.java
+++ Electiva2/crudUSCOSpring/src/main/java/crudPersonas/demo/controller/ControllerPersona.java
@@ -6,8 +6,8 @@ import org.springframework.ui.Model;
 import crudPersonas.demo.interfaceService.IPersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +25,25 @@ public class ControllerPersona {
         model.addAttribute("personas", personas);
         return "index";
     }
+
+    @GetMapping("eliminar/{id}")
+    public String delete(@PathVariable int id){
+        personaService.delete(id);
+        return "redirect:/listar";
+    }
+
+    @PostMapping("/save")
+    public String save(@Validated Personas p){
+        personaService.save(p);
+        return "redirect:/listar";
+    }
+
+    @GetMapping("/nuevo")
+    public String agregar(Model model){
+        model.addAttribute("persona", new Personas());
+        return "form";
+    }
+
+
+
 }
